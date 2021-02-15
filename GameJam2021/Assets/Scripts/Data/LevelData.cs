63b6cc5191c4d9bca26d5c2d6e64a72559bd8665
Mitul514 <mitulpradhan10@gmail.com>
2021-02-15 23:18:44 +0530

    Enum Types Added

---
+++ GameJam2021/Assets/Scripts/Data/LevelData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level Data", menuName = "Data/LevelData")]
+public class LevelData : ScriptableObject
+{
+  public string LevelName;
+
+  public NpcData[] NpcCount;
+  
+  public float PlayerSpeed;
+}
+
+[System.Serializable]
+public class NpcData
+{
+  public string NpcName;
+  public float NpcSpeed;
+  public NpcType Type;
+}
