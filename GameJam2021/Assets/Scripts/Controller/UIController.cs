2a8b6e840816634bc04c7e1a3897c3e6845fe67b
Mitul514 <mitulpradhan10@gmail.com>
2021-02-16 11:45:28 +0530

    Basic State Creation Done

--- GameJam2021/Assets/Scripts/Controller/UIController.cs
+++ GameJam2021/Assets/Scripts/Controller/UIController.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public static UIController Instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  [SerializeField] private Button _playButton;
+  [SerializeField] private Button _optionButton;
+  [SerializeField] private Button _exitButton;
+
+  private void Awake()
+  {
+    Instance = this;
+  }
+
+  private void Start()
+  {
+    GameManager.Instance.MenuTrigger += ShowMenu;
+  }
+
+  private void OnDestroy()
+  {
+    GameManager.Instance.MenuTrigger -= ShowMenu;
+  }
+
+  private void ShowMenu()
+  {
+    InstantiateMenu();
+    _playButton.onClick.AddListener(OnPlayClick);
+    _optionButton.onClick.AddListener(OnOptionClick);
+    _exitButton.onClick.AddListener(OnExitClick);
+  }
+
+  private void InstantiateMenu()
+  {
+  }
+
+  private void OnExitClick()
+  {
+  }
+
+  private void OnOptionClick()
+  {
+  }
+
+  private void OnPlayClick()
+  {
+    GameManager.Instance._states = States.GameState;
+  }
 }
---
+++ GameJam2021/Assets/Scripts/Services/PlayerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerService : MonoBehaviour
+{
+
+  private void Start()
+  {
+    GameController.Instance.LoadPlayer += SetPlayerData;
+  }
+
+  private void OnDestroy()
+  {
+    GameController.Instance.LoadPlayer -= SetPlayerData;
+  }
+
+  private void SetPlayerData()
+  {
+    //player behaviour creation
+    //movement method create
+    //collision detection create
+  }
+}
